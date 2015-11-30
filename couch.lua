3137e0079d565c0e4ba43585d42b197dd5285acd
Sean Conner <spc@conman.org>
2015-11-30 09:56:03 -0500

    Add licensing information for each file.
    The ones I wrote are GPL.  The ones I didn't I don't know the licensing
    agreement, but did find on the net.  Use at your own risk.

--- couch.lua
+++ couch.lua
@@ -1,5 +1,33 @@
 #!/usr/bin/env lua
-
+-- ***************************************************************
+--
+-- Copyright 2015 by Sean Conner.  All Rights Reserved.
+-- 
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or (at your
+-- option) any later version.
+-- 
+-- This library is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+-- License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this library; if not, see <http://www.gnu.org/licenses/>.
+--
+-- Comments, questions and criticisms can be sent to: sean@conman.org
+--
+-- ====================================================================
+--
+-- This program runs both Eliza and Racter.  You'll need to install the
+-- packages from https://github.com/spc476/lua-conmanorg to run this.
+--
+-- There may be other things you need to change to run this successfully.
+--
+-- You have been warned.
+--
+-- ********************************************************************
 local errno   = require "org.conman.errno"
 local fsys    = require "org.conman.fsys"
 local process = require "org.conman.process"
